9e250a20c161a25f4655fcc6ac944d4f86f2b62f
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-05-30 15:43:24 +0200

    add dependencies within function

--- src/dotools_py/pp/cellbender.py
+++ src/dotools_py/pp/cellbender.py
@@ -3,13 +3,6 @@ import subprocess
 
 import logger
 import numpy as np
-import rpy2.robjects as robjects
-import scanpy as sc
-from rpy2.robjects import numpy2ri, pandas2ri
-from rpy2.robjects.conversion import localconverter
-from rpy2.robjects.packages import importr
-from utils import convert_path, get_paths_utils
-
 
 def _run_barcoderanks(adata):
     """Run BarcodeRanks from DropletUtils to estimate the lower and upper bound of cells to use to estimate cell probabilities.
@@ -17,6 +10,13 @@ def _run_barcoderanks(adata):
     :param adata: anndata object with raw counts in X. Should be the raw matrix from cellranger
     :return: lower and upper bound
     """
+    import rpy2.robjects as robjects
+    import scanpy as sc
+    from rpy2.robjects import numpy2ri, pandas2ri
+    from rpy2.robjects.conversion import localconverter
+    from rpy2.robjects.packages import importr
+    from utils import convert_path, get_paths_utils
+
     numpy2ri.activate()
     pandas2ri.activate()
     dropletutils = importr("DropletUtils")
