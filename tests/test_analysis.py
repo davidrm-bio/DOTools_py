20b96fa8736821cd5170a9b35879a24b83f7fd74
davidrm-bio <davidtesorillo@hotmail.com>
2025-05-31 14:06:39 +0200

    misc

---
+++ tests/test_analysis.py
@@ -0,0 +1,4 @@
+from dotools_py.tl import get_expr
+
+def test_get(adata):
+    data = get_expr(adata, 'gene1', 'group')
--- tests/test_dotplot.py
+++
@@ -1,8 +0,0 @@
-from dotools_py.pl import dotplot
-from dotools_py._settings import iOff
-
-
-
-def test_dotplot(adata):
-    iOff()
-    dotplot(adata, 'group', 'gene1')
