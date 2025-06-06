57b9027a387a3758b65625de95e7e92a30765cd6
davidrm-bio <davidtesorillo@hotmail.com>
2025-06-06 18:34:10 +0200

    Update docstring and make parameters optional

--- src/dotools_py/pp/_cellbender.py
+++ src/dotools_py/pp/_cellbender.py
@@ -1,24 +1,27 @@
 import os
 import subprocess
+from typing import Union
+import anndata as ad
 
 import numpy as np
-
+import scanpy as sc
 from dotools_py import logger
+from dotools_py.utils import get_paths_utils, convert_path
 
 
-def _run_barcoderanks(adata):
-    """Run BarcodeRanks from DropletUtils to estimate the lower and upper bound of cells to use to estimate cell probabilities.
+def _run_barcoderanks(
+    adata: ad.AnnData
+) -> tuple[int, int]:
+    """Run BarcodeRanks from DropletUtils to estimate the lower and upper bound of true cells.
 
-    :param adata: anndata object with raw counts in X. Should be the raw matrix from cellranger
+    :param adata: annotated dt matrix of shape with `.X` containing raw counts.
     :return: lower and upper bound
     """
     import rpy2.robjects as robjects
-    from rpy2.robjects import numpy2ri, pandas2ri
+    from rpy2.robjects import numpy2ri
     from rpy2.robjects.conversion import localconverter
     from rpy2.robjects.packages import importr
 
-    numpy2ri.activate()
-    pandas2ri.activate()
     dropletutils = importr("DropletUtils")
 
     x_py = adata.X.T
@@ -43,35 +46,53 @@ def _run_barcoderanks(adata):
 def run_cellbender(
     cellranger_path: str,
     output_path: str,
-    samplenames: list = None,
+    samplenames: Union[list, None] = None,
     cuda: bool = True,
     cpu_threads: int = 15,
     epochs: int = 150,
     lr: float = 0.00001,
     estimator_multiple_cpu: bool = False,
     log: bool = True,
-    conda_path: str = None,
+    conda_path: Union[str, None] = None,
+    run_dropletutils: bool = False,
 ) -> None:
     """Run cellbender to remove ambient RNA.
 
-    :param cellranger_path:
-    :param output_path:
-    :param samplenames:
-    :param cuda:
-    :param cpu_threads:
-    :param epochs:
-    :param lr:
-    :param estimator_multiple_cpu:
-    :param log:
-    :param conda_path:
-    :return:
+    Remove ambient RNA using `cellbender <https://cellbender.readthedocs.io/en/latest/>`_. Assumes that the FASTQ files
+    have been mapped with CellRanger.
+
+    .. warning::
+        It is recommended to have access to GPU when running cellbender. Running CellBender on
+        CPU might lead to high running time.
+
+    :param cellranger_path: path to folder containing subfolders for each sample.
+    :param output_path: output folder to save the H5 files with the corrected expression matrix.
+    :param samplenames: list with the name of the folders in `cellranger_path`. If not set, it will be infered.
+    :param cuda: set to True to use GPU for the training.
+    :param cpu_threads: number of CPUs to use for training.
+    :param epochs: number of epochs to train for. The default number is 150, higher number might lead to overfitting.
+    :param lr: learning rate.
+    :param estimator_multiple_cpu: use multiple CPUs for the generation of results. It is not recommended for big datasets.
+    :param log: generate a log file with the stdout from running CellBender.
+    :param conda_path: path to the conda environment with cellbender installed. If not provided, a conda environment will
+                       be created in `~/.venv/cellbender`.
+    :param run_dropletutils: run DropletUtils to estimate the expected number of cells and total number of droplets to
+                             use as a prior for cellbender.
+    :return: H5 files with the corrected expression matrix will be saved in the output folder
+
+    Example
+    --------
+    >>> import dotools_py as do
+    >>> in_path = '/path/to/cellranger'
+    >>> out_path = '/path/to/output'
+    >>> do.pp.run_cellbender(in_path, out_path)
     """
     # Check-Ups and Information
     samplenames = [samplenames] if isinstance(samplenames, str) else samplenames
     assert os.path.exists(cellranger_path), f"{cellranger_path} does not exist"
     assert os.path.exists(output_path), f"{cellranger_path} does not exist"
 
-    bash_script = get_paths_utils("run_CellBender.sh")
+    bash_script = get_paths_utils("_run_CellBender.sh")
 
     if estimator_multiple_cpu:
         logger.info("Estimator_multiple_cpu is set to True, this is not recommended for big datasets >20-30k cells")
@@ -107,11 +128,14 @@ def run_cellbender(
         samples = samplenames
 
     logger.info(f"Running cellbender for {len(samples)} samples")
+    expected_cells, total_droplets = None, None
     for batch in samples:
         # Run one by one but sequentially
         # Estimate the number of cells to be used as upper and lower bound
-        tdata = sc.read_10x_h5(cellranger_path / batch / "outs" / "raw_feature_bc_matrix.h5")
-        expected_cells, total_droplets = _run_barcoderanks(tdata)  # Run with rpy2; gives a good estimate
+        if run_dropletutils:
+            file = next((cellranger_path / batch / "outs").glob("*raw_feature_bc_matrix.h5"))
+            tdata = sc.read_10x_h5(file)
+            expected_cells, total_droplets = _run_barcoderanks(tdata)  # Run with rpy2; gives a good estimate
 
         command = [
             "conda",
@@ -127,23 +151,25 @@ def run_cellbender(
             "--cellRanger-output",
             cellranger_path,
             "--cpu-threads",
-            cpu_threads,
+            str(cpu_threads),
             "--epochs",
-            epochs,
+            str(epochs),
             "--lr",
-            lr,
+            str(lr),
             "--expected-cells",
-            expected_cells,
+            str(expected_cells),
             "--total-droplets",
-            total_droplets,
+            str(total_droplets),
         ]
-
-        command += ["--cuda"] if cuda else None
-        command += ["--log"] if log else None
-        command += ["--estimator_multiple_cpu"] if estimator_multiple_cpu else None
+        command += ["--expected-cells", expected_cells] if expected_cells is not None else []
+        command += ["--total-droplets", total_droplets] if total_droplets is not None else []
+        command += ["--cuda"] if cuda else []
+        command += ["--log"] if log else []
+        command += ["--estimator_multiple_cpu"] if estimator_multiple_cpu else []
 
         try:
-            subprocess.run(command, check=True)
+            logger.info('Running Cellbender, might take a while')
+            subprocess.run(command, check=True, cwd=output_path)
         except subprocess.CalledProcessError as e:
             logger.info(f"Error running CellBender in conda environment: {e}")
 
