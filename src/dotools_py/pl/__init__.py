f249dabdde0d8f0251d685c9f5d7e479b834c494
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-06-07 01:09:15 +0200

    correct import

--- src/dotools_py/pl/__init__.py
+++ src/dotools_py/pl/__init__.py
@@ -1,5 +1,5 @@
 from dotools_py.pl._dotplot import dotplot
-from dotools_py.pl._embeddings import embedding, umap
+from dotools_py.pl._embeddings import embedding, umap, split_embeddding
 from dotools_py.pl._downstream import cell_props, split_bar_gsea, volcano_plot
 from dotools_py.pl._visium import slides, layers
 from dotools_py.pl._expression import barplot, violin, boxplot
