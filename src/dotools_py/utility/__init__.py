3f0a1efedc5e6318a253860fc0776491c4a63769
davidrm-bio <davidtesorillo@hotmail.com>
2025-06-12 17:29:40 +0200

    correct bugs

--- src/dotools_py/utility/__init__.py
+++ src/dotools_py/utility/__init__.py
@@ -1,3 +1,3 @@
-from dotools_py.utility._general import free_memory
+from dotools_py.utility._general import free_memory, transfer_labels
 from dotools_py.utility._spatial import select_slide, add_smooth_kernel
-from dotools_py.utility._plotting import generate_cmap, extended_tab20, get_hex_colormaps
+from dotools_py.utility._plotting import generate_cmap, extended_tab20, get_hex_colormaps, spine_format
