aaca4ba2900768ee1679e1e3a0d74a98a6e862bb
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-06-23 11:58:15 +0200

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -116,7 +116,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
-html_logo = "docs/_static/figures/LogoDoTools.png"
+html_logo = "docs/figures/LogoDoTools.png"
 nbsphinx_allow_errors = True
 
 html_title = project_name
