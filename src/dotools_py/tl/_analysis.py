13e148235e31275123d91e6eb719b8da087c9548
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-07-15 13:43:48 +0200

    Improve docstrings

--- src/dotools_py/tl/_analysis.py
+++ src/dotools_py/tl/_analysis.py
@@ -339,7 +339,7 @@ def auto_annot(
     :param pl_cell_prob: Generate a Dotplot to visualise the cell probabilities for each cluster.
     :param path: Path to save the dotplot of cell probabilities.
     :param filename: Name of the file.
-    :return: Returns `None`. The following fields will be set:
+    :return: Return `None`. The following fields will be set:
 
             `adata.obs['autoAnnot' | key_added]`: :class:`pandas.Series` (dtype ``category``)
                 Array that stores the predicted annotation for each cell.
