a8e4dc436b3da83a069b474875bd2909c5d37c7e
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-07-30 09:40:59 +0200

    Update configuration file

--- src/dotools_py/settings/_configure.py
+++ src/dotools_py/settings/_configure.py
@@ -163,7 +163,11 @@ def session_settings(
             "savefig.bbox": "tight",  # Remove unnecessary whitespace
         }
     )
-    sc.settings._vector_friendly = vector_friendly
+
+    sc.set_figure_params(scanpy=False, dpi_save=dpi_save, dpi=dpi,
+                         vector_friendly=vector_friendly, fontsize=fontsize,
+                         color_map=colormap, figsize=figsize, facecolor=facecolor,
+                         transparent=transparent)
     mpl.rcParams["pdf.fonttype"] = 42  # Use TrueType fonts in PDFs (editable text)
 
     return None
