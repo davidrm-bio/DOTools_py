5ac7d9cfbb7f35447f2e608964c1be18afde6f80
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-10-02 20:21:39 +0200

    Implement test for the tool module

---
+++ tests/tools/test_analysis.py
@@ -0,0 +1,62 @@
+import dotools_py as do
+import pandas as pd
+import anndata as ad
+
+
+def test_autoannot():
+    adata = do.dt.example_10x_processed()
+
+    del adata.obs["autoAnnot"]
+    do.tl.auto_annot(adata, "leiden", convert=False)
+    assert "autoAnnot" in adata.obs.columns
+
+    return None
+
+
+
+def test_full_recluster():
+    adata = do.dt.example_10x_processed()
+
+    do.tl.full_recluster(adata, "leiden", batch_key="batch",
+                         recluster_apporach="cca5", use_rep="X_CCA", resolution=1)
+
+    assert "annotation_fullrecluster" in adata.obs.columns
+    assert len(adata.obs["annotation_fullrecluster"].unique()) > len(adata.obs["leiden"].unique())
+
+    return None
+
+
+def test_integrate():
+    adata = do.dt.example_10x_processed()
+
+    # Harmony Integration
+    do.tl.integrate_data(adata, batch_key="batch", harmony=True)
+    assert "X_harmony" in adata.obsm.keys()
+
+    # BBKNN Integration
+    keys = list(adata.obsm.keys())
+    for key in keys:
+        if key == "X_pca":
+            continue
+        del adata.obsm[key]
+    do.tl.integrate_data(adata, batch_key="batch", bbknn=True)
+    assert "X_umap" in adata.obsm.keys()
+
+    # scVI Integration
+    do.tl.integrate_data(adata, batch_key="batch", scvi=True)
+    assert "X_scVI" in adata.obsm.keys()
+
+    return None
+
+
+def test_reclustering():
+    adata = do.dt.example_10x_processed()
+
+    counts = adata.obs.value_counts("annotation")
+    adata_subset  = do.tl.reclustering(adata, "annotation", "batch", "cca5",
+                                       use_rep="X_CCA", use_clusters=["B_cells"], get_subset=True)
+    assert isinstance(adata_subset, ad.AnnData)
+    assert adata_subset.n_obs == counts["B_cells"]
+    return None
+
+
---
+++ tests/tools/test_statistic.py
@@ -0,0 +1,77 @@
+import dotools_py as do
+import pandas as pd
+import anndata as ad
+
+
+def test_enrichr():
+    adata = do.dt.example_10x_processed()
+
+    do.tl.rank_genes_groups(adata, "condition")
+    table = do.get.dge_results(adata)
+    df = do.tl.go_analysis(table, gene_key="GeneName", pval_key="padj", log2fc_key="log2fc")
+
+    assert isinstance(df, pd.DataFrame)
+    cols = {'Gene_set', 'Term', 'Overlap', 'P-value', 'Adjusted P-value', 'Old P-value', 'Old Adjusted P-value',
+            'Odds Ratio', 'Combined Score', 'Genes', 'state'}
+    assert cols.issubset(df.columns)
+    return None
+
+
+def test_rank_genes_groups():
+    adata = do.dt.example_10x_processed()
+
+    do.tl.rank_genes_groups(adata, "condition")
+    assert "rank_genes_groups" in adata.uns.keys()
+
+    return  None
+
+
+def test_ttest():
+    import random
+    adata = do.dt.example_10x_processed()
+
+    # Generate pseudoreplicates
+    batches = [f"batch{i}" for i in range(1, 7)]
+    adata.obs["batch_technical"] = random.choices(batches, k=adata.n_obs)
+
+    do.tl.grouped_ttest(adata, batch_key="batch_technical")
+    assert "grouped_ttest" in adata.uns.keys()
+    df = adata.uns["grouped_ttest"]
+    cols = {"gene", "annotation", "condition",  "pval", "statistic"}
+    assert cols.issubset(df.columns)
+    assert df["pval"].max() <=1
+    assert df["pval"].min() >=0
+    return None
+
+
+def test_pseudobulking():
+    adata = do.dt.example_10x_processed()
+    adata.obs.value_counts(["condition", "annotation"])
+
+    pdata = do.tl.pseudobulking(adata, "condition", "annotation", min_cells=0, min_counts=0)
+
+    assert isinstance(pdata, ad.AnnData)
+    assert pdata.n_obs == 10
+    return None
+
+
+def test_rank_genes_condition():
+    adata = do.dt.example_10x_processed()
+
+    df = do.tl.rank_genes_condition(adata, groupby="condition", subset_by="annotation", reference="healthy")
+
+    assert isinstance(df, pd.DataFrame)
+    assert "rank_genes_condition" in adata.uns.keys()
+    cols = {'GeneName', 'wilcox_score', 'log2fc', 'pvals', 'padj', 'pts_group', 'pts_ref', 'group', 'annotation'}
+    assert cols.issubset(df.columns)
+    return None
+
+
+# The following tests:
+# test_rank_genes_consensus
+# test_rank_genes_pseudobulk
+# test_run_mast
+# require R, which is not set-up to be installed in the server, therefore we do not implement a test for
+# these functions
+
+# 12 functions in do.tl with, 9 test implemented (75 % coverage)
