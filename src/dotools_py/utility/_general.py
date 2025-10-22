907f070ce67a1afae6e56e7b2f6780e425bb1081
David Rodriguez Morales <davidtesorillo@hotmail.com>
2025-10-22 13:16:36 +0200

    Update RDS to AnnData to RDS convertion

--- src/dotools_py/utility/_general.py
+++ src/dotools_py/utility/_general.py
@@ -1,18 +1,14 @@
 import os.path
 from pathlib import Path
-import uuid
-import subprocess
-from typing import Literal, Union
+import platform
+from typing import Literal
 import gzip
 import pickle
 
 import anndata as ad
 import pandas as pd
-import polars as pl
-import scipy.sparse as sp
 
-from dotools_py import logger
-from dotools_py.utils import get_paths_utils
+from dotools_py.utility._language import RDSConverter
 
 HERE = Path(__file__).parent
 
@@ -26,9 +22,12 @@ def free_memory() -> None:
     import gc
 
     gc.collect()
-    try:
+
+    system = platform.system()
+
+    if system == "Linux":
         ctypes.CDLL("libc.so.6").malloc_trim(0)
-    except OSError:
+    else:
         pass
     return None
 
@@ -70,17 +69,25 @@ def transfer_labels(
         return None
 
 
-def read_rds(path_rds: str | Path, path_adata: str | Path, batch_key: str = 'batch') -> ad.AnnData:
+def read_rds(
+    path_rds: str | Path,
+    path_h5ad: str | Path = None,
+    filename_h5ad: str = "adata.h5ad",
+    batch_key: str = "batch",
+    rds_batch_key: str = "orig.ident",
+) -> ad.AnnData | None:
     """Read Rds object with Seurat or SingleCellExperiment Object.
 
     .. note::
         When reading an RDS Object with counts and logcounts data, the counts will be returned in the
         `X` attribute, while the logcounts are returned as a layer.
 
-    :param path_rds: path to RDS file with SingleCellExperiment or SeuratObject.
-    :param path_adata: path to save AnnData Object including the filename.
-    :param batch_key: name in `obs` to save batch information.
-    :return: Returns an `AnnData` Object. The AnnData is also saved under `path_adata`.
+    :param path_rds: Path to RDS file with SingleCellExperiment or SeuratObject.
+    :param path_h5ad: Path to save AnnData Object.
+    :param filename_h5ad: Name of the AnnData file.
+    :param batch_key: Name in `obs` to save batch information.
+    :param rds_batch_key: Name in the metadata column of the Seurat Object to save the batch information.
+    :return: Returns an `AnnData` Object or `None`. The AnnData can also be saved under `path_adata`.
 
     See Also
     --------
@@ -106,108 +113,40 @@ def read_rds(path_rds: str | Path, path_adata: str | Path, batch_key: str = 'bat
         obsp: 'connectivities', 'distances'
 
     """
-    rscript = get_paths_utils("_ReadWrite_RDS.R")
+    converter = RDSConverter(input_obj=path_rds,
+                             out_obj="anndata",
+                             batch_key=batch_key,
+                             rds_batch_key=rds_batch_key,
+                             path=None,
+                             filename=None,
+                             get_anndata=True
+                             )
+    adata = converter.to_h5ad()
 
-    cmd = [
-        "Rscript",
-        rscript,
-        "--input=" + str(path_rds),
-        "--out=" + str(path_adata),
-        "--operation=" + "read",
-        "--batch_key=" + batch_key
-    ]
-
-    logger.info("Reading the RDS")
-    subprocess.call(cmd)
-    logger.info("Generating AnnData Object")
-    adata = ad.read_h5ad(path_adata)
-
-    # Transfer missing information
-    input_folder = str(path_rds).split("/")
-    input_folder = input_folder[:-1]
-    input_folder = "/".join(input_folder)
-
-    # Variable Features
-    try:
-        hvg = pd.read_csv(os.path.join(input_folder, "VariableFeatures.csv")).set_index("Unnamed: 0")
-        logger.info("Transferring HVGs")
-        hvg_bool = [True if g in list(hvg["hvg"]) else False for g in adata.var_names]
-        adata.var["highly_variable"] = hvg_bool
-    except FileNotFoundError as e:
-        logger.info(f"Problem transferring HVGs, {e}")
-
-
-    # Connectivities
-    try:
-        connectivities = pl.read_csv(os.path.join(input_folder, "Connectivities.csv"), has_header=True, dtypes={bc: pl.Float64 for bc in adata.obs_names})
-        connectivities = connectivities.to_pandas()
-        if "" in connectivities.columns:
-            del connectivities[""]  # Index
-        if connectivities.shape[0] == connectivities.shape[1]:
-            logger.info("Transferring connectivities")
-            adata.obsp["connectivities"] = sp.csr_matrix(connectivities.values)
-        else:
-            logger.info("Problem transferring connectivities")
-    except FileNotFoundError as e:
-        logger.info(f"Problem transferring connectivities, {e}")
-
-    # Distances
-    try:
-        distances = pl.read_csv(os.path.join(input_folder, "Distances.csv"), has_header=True, dtypes={bc: pl.Float64 for bc in adata.obs_names})
-        distances = distances.to_pandas()
-        if "" in distances.columns:
-            del distances[""]  # Index
-        if distances.shape[0] == distances.shape[1]:
-            logger.info("Transferring neighbor distances")
-            adata.obsp["distances"] = sp.csr_matrix(distances.values)
-        else:
-            logger.info("Problem transferring neighbor distances")
-    except FileNotFoundError as e:
-        logger.info(f"Problem transferring distances, {e}")
-
-    # Rename reductions
-    logger.info("Renaming reductions")
-    obsm_keys = [key for key in adata.obsm.keys()]
-    for key in obsm_keys:
-        new_key = "X_" + key.lower().replace(".", "_").replace("-", "_")
-        adata.obsm[new_key] = adata.obsm[key].values
-        del adata.obsm[key]
-
-    # Rename orig.ident if present
-    if "orig.ident" in list(adata.obs.columns):
-        logger.info(f"Renaming orig.ident to {batch_key}")
-        adata.obs[batch_key] = adata.obs["orig.ident"].copy()
-        del adata.obs["orig.ident"]
-
-    # Default is X with raw counts
-    if all(adata.X.data % 1 == 0):
-        adata.layers["counts"] = adata.X.copy()
-
-    # Remove all intermediate files
-    for f in ["Distances.csv", "Connectivities.csv", "VariableFeatures.csv" ]:
-        try:
-            os.remove(os.path.join(input_folder, f))
-        except FileNotFoundError:
-            continue
-    logger.info("Done")
-    return  adata
+    if path_h5ad is not None:
+        adata.write(Path(path_h5ad) / filename_h5ad)
+    return adata
 
 
 def save_rds(
     path_rds: str,
-    batch_key: str,
+    filename_rds: str,
+    batch_key: str = "batch",
+    rds_batch_key: str = "orig.ident",
     adata: ad.AnnData = None,
     path_adata: str = None,
-    object_type: Literal["SingleCellExperiment", "SeuratObject"] = "SeuratObject",
+    out_type: Literal["seurat", "sce"] = "seurat",
 ) -> None:
     """Save AnnData as Seurat or SingleCellExperiment Object.
 
-    :param path_rds: Path to save RDS Object including filename.
+    :param path_rds: Path to save RDS Object.
+    :param filename_rds: Name of the RDS file.
     :param batch_key: Name in `obs` with batch information.
+    :param rds_batch_key: Name in the metadata column of the Seurat Object to save the batch information.
     :param adata: AnnData object
-    :param path_adata: Path to AnnData Object including the filename.
-    :param object_type: Specify the type of object that the AnnData should be converted to.
-    :return: Generate an RDS file in `path_rds` containing the Seurat or SingleCellExperiment Object.
+    :param path_adata:  Path to AnnData Object including the filename.
+    :param out_type: Specify the type of object that the AnnData should be converted to.
+    :return: Returns `None`. Generate an RDS file in `path_rds` containing the Seurat or SingleCellExperiment Object.
 
     See Also
     --------
@@ -218,7 +157,7 @@ def save_rds(
     >>> import dotools_py as do
     >>> import os
     >>> adata = do.dt.example_10x_processed()
-    >>> do.utility.save_rds(path_rds="/tmp/Seurat.rds", adata=adata, object_type="SeuratObject", batch_key="batch")
+    >>> do.utility.save_rds(path_rds="/tmp/Seurat.rds", adata=adata, object_type="seurat", batch_key="batch")
     >>> os.path.exists("/tmp/Seurat.rds")
     True
 
@@ -235,72 +174,30 @@ def save_rds(
             1851 features across 700 samples within 1 assay
             Active assay: RNA (1851 features, 191 variable features)
             2 layers present: counts, data
-            3 dimensional reductions calculated: cca, pca, umap
+            3-dimensional reductions calculated: cca, pca, umap
 
     """
-    rscript = get_paths_utils("_ReadWrite_RDS.R")
 
-    assert not (adata is not None and path_adata is not None), "Provide an AnnData or the path to an AnnData Object not both"
-    assert object_type in ["SeuratObject", "SingleCellExperiment"], "Specify the object type for the RDS 'SingleCellExperiment' or 'SeuratObject''"
-
-    if adata is not None:
-        path_adata = Path("/tmp") / f"Convertion_{uuid.uuid4().hex}"
-        path_adata.mkdir(parents=True, exist_ok=False)
-        del adata.uns, adata.raw
-        adata.write(path_adata / "adata.h5ad")
-        path_adata = os.path.join(path_adata, 'adata.h5ad')
-    else:
+    if adata is None:
         adata = ad.read_h5ad(path_adata)
 
-    # Save intermediate files
-    input_folder = str(path_adata).split("/")
-    input_folder = input_folder[:-1]
-    input_folder = "/".join(input_folder)
-
-    # distances --> nn
-    if "distances" in adata.obsp:
-        df = pl.DataFrame(adata.obsp["distances"].toarray())
-        df.columns = adata.obs_names
-        df.write_csv(os.path.join(input_folder,  "Distances.csv"))
-    if "connectivities" in adata.obsp:
-        # connectivities --> snn
-        df = pl.DataFrame(adata.obsp["connectivities"].toarray())
-        df.columns = adata.obs_names
-        df.write_csv(os.path.join(input_folder, "Connectivities.csv"))
-    if "highly_variable" in adata.var.columns:
-        # HVGs
-        hvg = adata.var.highly_variable
-        hvg.to_csv(os.path.join(input_folder, "VariableFeatures.csv"))
-
-    cmd = [
-        "Rscript",
-        rscript,
-        "--input=" + str(path_adata),
-        "--out=" + str(path_rds) ,
-        "--type=" + object_type,
-        "--operation=" + "write",
-        "--batch_key=" + batch_key
-    ]
-
-    logger.info(f"Generating the {object_type}")
-    subprocess.call(cmd)
-
-
-    # Remove all intermediate files
-    for f in ["Distances.csv", "Connectivities.csv","VariableFeatures.csv"]:
-        try:
-            os.remove(os.path.join(input_folder, f))
-        except FileNotFoundError:
-            continue
-
-    logger.info("Done")
+    converter = RDSConverter(input_obj=adata,
+                             out_obj=out_type,
+                             batch_key=batch_key,
+                             rds_batch_key=rds_batch_key,
+                             path=path_rds,
+                             filename=filename_rds,
+                             get_anndata=False
+                             )
+    converter.to_rds()
     return None
 
 
-def add_gene_metadata(data: Union[pd.DataFrame, ad.AnnData],
-                      gene_key: str,
-                      species: Literal["mouse", "human"] = "mouse"
-                      ) -> Union[pd.DataFrame, ad.AnnData]:
+def add_gene_metadata(
+    data: ad.AnnData | pd.DataFrame,
+    gene_key: str,
+    species: Literal["mouse", "human"] = "mouse"
+) -> ad.AnnData | pd.DataFrame:
     """Add gene metadata to AnnData or DataFrame.
 
     Add gene metadata obtained from the GTF or Uniprot-database. This information includes,
@@ -352,8 +249,8 @@ def add_gene_metadata(data: Union[pd.DataFrame, ad.AnnData],
         data_copy["locations"] = [",".join(database[g]["locations"]) if g in database else "NaN" for g in genes]
         data_copy["gene_id"] = [database[g]["gene_id"] if g in database else "NaN" for g in genes]
     elif isinstance(data_copy, ad.AnnData):
-        genes = list(data_copy.var_names)  if gene_key == "var_names" else data_copy.var[gene_key].tolist()
-        data_copy.var["biotype"] = [database[g]["gene_biotype"] if g in database else "NaN" for g in genes]
+        genes = list(data_copy.var_names) if gene_key == "var_names" else data_copy.var[gene_key].tolist()
+        data_copy.var["biotype"] = [database[g]["gene_type"] if g in database else "NaN" for g in genes]
         data_copy.var["locations"] = [",".join(database[g]["locations"]) if g in database else "NaN" for g in genes]
         data_copy.var["gene_id"] = [database[g]["gene_id"] if g in database else "NaN" for g in genes]
     else:
